d8bd4127ea03ffd468ced8185bb007c0f20445e5
zerin5577 <zerin5577@gmail.com>
2017-06-26 14:29:41 -0700

    mysite/

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -17,8 +17,15 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+    # def was_published_recently(self):
+    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 @python_2_unicode_compatible # only if you need to support Python 2
 class Choice(models.Model):
@@ -27,3 +34,7 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
